0c5252eb793fd1b237b56fb97003ba2adff4a849
KoKoLates <the21515@gmail.com>
2024-08-24 21:28:23 +0800

    chore(cli): update command-lime interface options handler

--- src/cli.js
+++ src/cli.js
@@ -15,4 +15,19 @@ const options = program.opts();
 if (options.file) {
   const code = fs.readFileSync(options.file, "utf-8");
   const ast = parser.parse(code);
+
+  if (options.transform) {
+    const transform_ast = transformer.apply_transform(ast, options.transform);
+    const new_code = transformer.generate_code(transform_ast);
+
+    if (options.output) {
+      fs.writeFileSync(options.output, new_code, "utf-8");
+      console.log("Output written to", options.output);
+    } else {
+      console.log(new_code);
+    }
+  } else {
+    console.log(Json.stringify(ast, null, 2));
+  }
+
 }
